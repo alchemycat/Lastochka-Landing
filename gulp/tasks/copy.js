4bb838f427fefc15c0de6061cac916757f892e7f
Владислав Тищенко <vladislavtishenko@MacBook-Air-Vladislav.local>
2023-05-09 16:43:26 +0300

    сделал хедер и адаптив до максимального разрешения 991px

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,4 +1,6 @@
 export const copy = () => {
     return app.gulp.src(app.path.src.files)
     .pipe(app.gulp.dest(app.path.build.files))
+    .pipe(app.gulp.src(`${app.path.src.js}/external/**/*`))
+    .pipe(app.gulp.dest(`${app.path.build.js}/external`));
 }
