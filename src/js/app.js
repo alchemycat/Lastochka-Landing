a68e9a3e2b2969a6f75831581cc55ded55a8d24b
Владислав Тищенко <vladislavtishenko@MacBook-Air-Vladislav.local>
2023-05-10 21:19:33 +0300

    добавил секцию calc + адаптив

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,11 @@
 import * as flsFunctions from "./modules/functions.js";
 import burgerMenu from "./modules/burger.js";
-
-import Swiper from 'swiper';
+import slider from "./modules/slider.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 	flsFunctions.isWebp();
 	
 	burgerMenu();
 
-	const swiper = new Swiper('.swiper', {
-		loop: true,
-		slidesPerView: 'auto',
-		autoplay: {
-		  delay: 5000,
-		  disableOnInteraction: true
-		},
-		centeredSlides: true,
-	  });
-
-	  const counter = document.querySelector('.interior__counter');
-
-	  function setSwiperCounter() {
-		counter.innerHTML = `${swiper.realIndex + 1}/${swiper.slides.length}`;
-	  }
-
-	  setSwiperCounter();
-
-	  swiper.on('slideChange', setSwiperCounter);
-	  
-	  const nextButton = document.querySelector('.swiper-arrow-next');
-	  const prevButton = document.querySelector('.swiper-arrow-prev');
-
-	  nextButton.addEventListener('click', nextSlide);
-	  prevButton.addEventListener('click', prevSlide);
-	  
-	  function nextSlide() {
-		swiper.slideNext();	
-	  }
-
-	  function prevSlide() {
-		swiper.slidePrev();
-	  }
-	  
+	slider(); 
 });
---
+++ src/js/modules/slider.js
@@ -0,0 +1,37 @@
+import Swiper from 'swiper';
+
+export default function slider() {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        slidesPerView: 'auto',
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: true
+        },
+        centeredSlides: true,
+      });
+
+      const counter = document.querySelector('.interior__counter');
+
+      function setSwiperCounter() {
+        counter.innerHTML = `${swiper.realIndex + 1}/${swiper.slides.length}`;
+      }
+
+      setSwiperCounter();
+
+      swiper.on('slideChange', setSwiperCounter);
+      
+      const nextButton = document.querySelector('.swiper-arrow-next');
+      const prevButton = document.querySelector('.swiper-arrow-prev');
+
+      nextButton.addEventListener('click', nextSlide);
+      prevButton.addEventListener('click', prevSlide);
+      
+      function nextSlide() {
+        swiper.slideNext();	
+      }
+
+      function prevSlide() {
+        swiper.slidePrev();
+      }
+}
